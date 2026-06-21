9ef613966b3990751a2d63d75584f2e64f9683c3
sanikthehedgehorg <samik.mookherjee@gmail.com>
2026-06-21 16:05:45 -0400

    Added a Dash and improved movement feel

--- Scripts/phys_player.gd
+++ Scripts/phys_player.gd
@@ -1,32 +1,62 @@
 extends RigidBody2D
 
-@export var move_force: float = 10000.0
-@export var move_force_ratio: float = .3
-@export var jump_impulse: float = 4000.0
+const move_force: float = 10000.0
+const jump_impulse: float = 700
+const veloDamp = 6
+const airVeloDamp = 1
+const dashSpeed = 2000
+const dashDuration = .4
+const move_force_ratio: float = .17
+const dashCooltime = .75
+var hasDJ = true
+var hasDash = true
+var dashDirection = Vector2(0,0)
+var onDashCool = false
+@onready var dashTimer: Timer = $dashTimer
+@onready var dashCoolTimer: Timer = $dashCoolTimer
 @onready var raycast :RayCast2D = $RayCast2D
-var veloDamp = 1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-	
+	dashTimer.timeout.connect(dashTimer_timeout)
+	dashCoolTimer.timeout.connect(dashCoolTimer_timeout)
+func dashTimer_timeout():
+	pass
+func dashCoolTimer_timeout():
+	onDashCool = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
+	if Input.is_action_just_pressed("Dash") and not dashTimer.time_left > 0 and hasDash and direction and not onDashCool:
+		hasDash = false
+		onDashCool = true
+		dashDirection = direction
+		dashTimer.start(dashDuration)
+		dashCoolTimer.start(dashCooltime)
 	if raycast.is_colliding():
-		move_force_ratio = .3
+		hasDJ = true
+		hasDash = true
 		if direction:
 			apply_central_force(Vector2(direction*move_force,0))
-		if Input.is_action_just_pressed("ui_accept"):
-			apply_central_impulse(Vector2(0,-jump_impulse))
+		apply_central_force(linear_velocity*veloDamp*-1)
 	else:
 		if direction:
 			apply_central_force(Vector2(direction*move_force*move_force_ratio,0))
-		if move_force_ratio > .1:
-			move_force_ratio -= .003
-	apply_central_force(linear_velocity*veloDamp*-1)
+		apply_central_force(linear_velocity*airVeloDamp*-1)
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	if dashTimer.time_left > 0:
+		state.linear_velocity = Vector2(dashDirection * dashSpeed * ((dashTimer.time_left  / dashDuration) + .4),0)
+	if raycast.is_colliding():
+		if Input.is_action_just_pressed("ui_accept"):
+			state.linear_velocity.y = -jump_impulse
+	else:
+		if Input.is_action_just_pressed("ui_accept") and hasDJ:
+			state.linear_velocity.y = -jump_impulse
+			hasDJ = false
+		
+		
+	
 	
 
 	
